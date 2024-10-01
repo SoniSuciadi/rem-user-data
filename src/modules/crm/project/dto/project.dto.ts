f22f381632c84f5194b36d3ad3548e913537ac2d
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-01 19:18:26 +0700

    Add API properties to CreateProjectDto fields

--- src/modules/crm/project/dto/project.dto.ts
+++ src/modules/crm/project/dto/project.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsString,
   IsEmail,
@@ -12,6 +13,63 @@ import {
 } from 'class-validator';
 
 export class CreateProjectDto {
+  @ApiProperty({
+    example: 'The Leaf Residence',
+  })
   @IsString()
   name: string;
+
+  @ApiProperty({
+    example: 'JL Sudirman etc...',
+  })
+  @IsString()
+  officeAddress: string;
+
+  @ApiProperty({
+    example: 'PT Indonesia Merdeka',
+  })
+  @IsString()
+  ptName: string;
+
+  @ApiProperty({
+    example: 'Jalan Sudirman etc...',
+  })
+  @IsString()
+  projectAddress: string;
+
+  @ApiProperty({
+    example: 'John Jackson',
+  })
+  @IsString()
+  accountNameBprs: string;
+
+  @ApiProperty({
+    example: '312341234',
+  })
+  @IsString()
+  accountNumberBprs: string;
+
+  @ApiProperty({
+    example: 'Bank Central Indonesia (BCA)',
+  })
+  @IsString()
+  bankNameBprs: string;
+
+  @ApiProperty({
+    example: 'John Jackson',
+  })
+  @IsString()
+  accountNameOther: string;
+
+  @ApiProperty({
+    example: '312341234',
+  })
+  @IsString()
+  accountNumberOther: string;
+
+  @ApiProperty({
+    example: 'Bank Central Indonesia (BCA)',
+  })
+  @IsString()
+  bankNameOther: string;
 }
