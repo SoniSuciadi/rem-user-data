6b7c93cc274579baa2ab57a922b5f03408348e9f
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-01 22:31:28 +0700

    Refactor database query in ProjectService module

--- src/modules/crm/project/project.service.ts
+++ src/modules/crm/project/project.service.ts
@@ -6,18 +6,21 @@ export class ProjectService {
     SELECT
       p.name,
       p."abbreviation",
-      COALESCE(json_extract(p."developerInformation", '$.address'), '') AS "officeAddress",
-      COALESCE(json_extract(p."developerInformation", '$.name'), '') AS "ptName",
-      COALESCE(json_extract(p."developerInformation", '$.projectAddress'), '') AS "projectAddress",
-      COALESCE(json_extract(p."bprs_bank_information", '$.accountName'), '') AS "accountNameBprs",
-      COALESCE(json_extract(p."bprs_bank_information", '$.accountNumber'), '') AS "accountNumberBprs",
-      COALESCE(json_extract(p."bprs_bank_information", '$.bankName'), '') AS "bankNameBprs",
-      COALESCE(json_extract(p."other_bank_information", '$.accountName'), '') AS "accountNameOther",
-      COALESCE(json_extract(p."other_bank_information", '$.accountNumber'), '') AS "accountNumberOther",
-      COALESCE(json_extract(p."other_bank_information", '$.bankName'), '') AS "bankNameOther",
       p."isExternal"
     FROM cms_projects p
     `;
+
+    // save query
+    // COALESCE(json_extract(p."developerInformation", '$.address'), '') AS "officeAddress",
+    // COALESCE(json_extract(p."developerInformation", '$.name'), '') AS "ptName",
+    // COALESCE(json_extract(p."developerInformation", '$.projectAddress'), '') AS "projectAddress",
+    // COALESCE(json_extract(p."bprs_bank_information", '$.accountName'), '') AS "accountNameBprs",
+    // COALESCE(json_extract(p."bprs_bank_information", '$.accountNumber'), '') AS "accountNumberBprs",
+    // COALESCE(json_extract(p."bprs_bank_information", '$.bankName'), '') AS "bankNameBprs",
+    // COALESCE(json_extract(p."other_bank_information", '$.accountName'), '') AS "accountNameOther",
+    // COALESCE(json_extract(p."other_bank_information", '$.accountNumber'), '') AS "accountNumberOther",
+    // COALESCE(json_extract(p."other_bank_information", '$.bankName'), '') AS "bankNameOther",
+
     const data = await dbPocketbase({ q });
     return data || [];
   }
