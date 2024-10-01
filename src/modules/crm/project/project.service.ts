ecc37647d59a169e5e119da15abd5f70165f7000
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-01 17:48:31 +0700

    Add SQL query to fetch project information from database

--- src/modules/crm/project/project.service.ts
+++ src/modules/crm/project/project.service.ts
@@ -1,6 +1,24 @@
+import axios from 'axios';
 export class ProjectService {
   async getProjects() {
-
+    const q = `
+    SELECT
+      p.id,
+      p.name,
+      p."subId",
+      p."abbreviation",
+      COALESCE(json_extract(p."developerInformation", '$.address'), '') AS "officeAddress",
+      COALESCE(json_extract(p."developerInformation", '$.name'), '') AS "ptName",
+      COALESCE(json_extract(p."developerInformation", '$.projectAddress'), '') AS "projectAddress",
+      COALESCE(json_extract(p."bprs_bank_information", '$.accountName'), '') AS "accountNameBprs",
+      COALESCE(json_extract(p."bprs_bank_information", '$.accountNumber'), '') AS "accountNumberBprs",
+      COALESCE(json_extract(p."bprs_bank_information", '$.bankName'), '') AS "bankNameBprs",
+      COALESCE(json_extract(p."other_bank_information", '$.accountName'), '') AS "accountNameOther",
+      COALESCE(json_extract(p."other_bank_information", '$.accountNumber'), '') AS "accountNumberOther",
+      COALESCE(json_extract(p."other_bank_information", '$.bankName'), '') AS "bankNameOther",
+      p."isExternal"
+    FROM cms_projects p
+    `;
     return [];
   }
 }
