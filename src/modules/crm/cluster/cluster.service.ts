6e6515758c5bebd3225df6d1467a566671b79405
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-01 22:00:19 +0700

    Refactor getClusters method to fetch data efficiently

--- src/modules/crm/cluster/cluster.service.ts
+++ src/modules/crm/cluster/cluster.service.ts
@@ -1,8 +1,19 @@
+import { dbPocketbase } from 'src/common/helpers/crm.helper';
+
 export class ClusterService {
   async getClusters() {
     const q = `
-    
-    `
-    return [];
+    SELECT
+      c."name",
+      c.id,
+      p."name" AS "project",
+      COALESCE(s."stageName", '') AS "tahap"
+    FROM cms_clusters c
+    JOIN cms_projects p ON c."projectId" = p.id
+    LEFT JOIN cms_stages s ON c."stageId" = s.id
+    ORDER BY p."name"
+    `;
+    const data = await dbPocketbase({ q });
+    return data || [];
   }
 }
