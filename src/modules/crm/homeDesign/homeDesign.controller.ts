e34f6ff3ba307838f4c00533990dc1ce8c010318
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-02 06:39:22 +0700

    Add CreateHomeDesign endpoint and service method

--- src/modules/crm/homeDesign/homeDesign.controller.ts
+++ src/modules/crm/homeDesign/homeDesign.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { HomeDesignService } from './homeDesign.service';
+import { CreateHomeDesignDto } from './dto/homeDesign.dto';
 
 @Controller('crm/home-design')
 export class HomeDesignController {
@@ -10,6 +11,15 @@ export class HomeDesignController {
     const data = await this.HomeDesignService.gethomeDesign();
     return {
       message: 'Success get list crm home design',
+      data,
+    };
+  }
+
+  @Post()
+  async createHomeDesign(@Body() body: CreateHomeDesignDto) {
+    const data = await this.HomeDesignService.createHomeDesign(body);
+    return {
+      message: 'Success create crm home design',
       data,
     };
   }
--- src/modules/crm/homeDesign/homeDesign.service.ts
+++ src/modules/crm/homeDesign/homeDesign.service.ts
@@ -1,4 +1,5 @@
 import { dbPocketbase } from 'src/common/helpers/crm.helper';
+import { CreateHomeDesignDto } from './dto/homeDesign.dto';
 
 export class HomeDesignService {
   async gethomeDesign() {
@@ -10,5 +11,36 @@ export class HomeDesignService {
     `;
     const data = await dbPocketbase({ q });
     return data || [];
+  }
+
+  async createHomeDesign(arg: CreateHomeDesignDto) {
+    const detailUnit = {
+      bakKM: '',
+      doorWindow: 'Kusen Aluminium',
+      finishingWall: 'Cat Dinding',
+      floor: '40cm x 40cm',
+      floorNumber: '1',
+      foundation: 'Batu Kali Menerus',
+      kusenKM: 'PVC',
+      listric: '2200',
+      plafon: 'Gypsum',
+      road: 'Paving',
+      roadWidth: '6.5',
+      roof: 'Rangka Atap Galvalum',
+      sanitize: 'Closet duduk',
+      securitySystem: 'CCTV 24 jam',
+      structure: 'Beton Bertulang',
+      wall: 'Bata Ringan',
+      wallType: 'Hebel',
+      waterSource: 'Sumur Bor / PAM',
+    };
+
+    const fasilitas = {
+      bedroom: 2,
+      carpot: 1,
+      kitchen: 1,
+      livingRoom: 1,
+      toilet: 1,
+    };
   }
 }
