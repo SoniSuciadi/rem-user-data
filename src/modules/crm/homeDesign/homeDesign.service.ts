aa0c1680fe6bb5e352e37e26d8200c5b412b7f4a
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-02 06:17:09 +0700

    Add database query to retrieve home designs

--- src/modules/crm/homeDesign/homeDesign.service.ts
+++ src/modules/crm/homeDesign/homeDesign.service.ts
@@ -1,5 +1,14 @@
+import { dbPocketbase } from 'src/common/helpers/crm.helper';
+
 export class HomeDesignService {
   async gethomeDesign() {
-    return [];
+    const q = `
+    SELECT
+      hd."homeDesignName" AS "name",
+      hd."typeUnit" AS "Tipe"
+    FROM cms_home_design hd
+    `;
+    const data = await dbPocketbase({ q });
+    return data || [];
   }
 }
