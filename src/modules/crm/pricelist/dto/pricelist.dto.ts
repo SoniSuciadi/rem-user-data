f8f84b790485c6f4f1e6101febb6ddc4d1e9ab78
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-02 12:03:42 +0700

    Add example data to ListTipeDto and CreatePricelistDto

--- src/modules/crm/pricelist/dto/pricelist.dto.ts
+++ src/modules/crm/pricelist/dto/pricelist.dto.ts
@@ -33,55 +33,70 @@ export class GetFormPricelist {
 }
 
 class ListHargaDto {
+  @ApiProperty({ example: 'Tahap 1 (Ruko)' })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: '000000000000008-000000000000020' })
   @IsString()
   id: string;
 
+  @ApiProperty({ example: false })
   @IsBoolean()
   sell: boolean;
 
+  @ApiProperty({ example: 0 })
   @IsNumber()
   price: number;
 
+  @ApiProperty({ example: 0 })
   @IsNumber()
   kprCosts: number;
 
+  @ApiProperty({ example: 0 })
   @IsNumber()
   notaryFee: number;
 
+  @ApiProperty({ example: '', required: false })
   @IsString()
   @IsOptional()
   keterangan: string;
 }
 
 class ListTipeDto {
+  @ApiProperty({ example: 'Standart' })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 0 })
   @IsNumber()
   amount: number;
 
+  @ApiProperty({ example: 'NUP' })
   @IsString()
   typeNUP: string;
 
+  @ApiProperty({ example: '', required: false })
   @IsString()
   @IsOptional()
   document: string;
 
+  @ApiProperty({ example: '', required: false })
   @IsString()
   @IsOptional()
   caraPemesanan: string;
 
+  @ApiProperty({ example: '', required: false })
   @IsString()
   @IsOptional()
   catatan: string;
 
+  @ApiProperty({ example: '', required: false })
   @IsString()
   @IsOptional()
   dokumenPersyaratanKPR: string;
 
+  @ApiProperty({ type: [ListHargaDto] })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => ListHargaDto)
@@ -89,15 +104,27 @@ class ListTipeDto {
 }
 
 export class CreatePricelistDto {
+  @ApiProperty({
+    example: 'PL TLR Oktober 2024',
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    example: 'hadgj1bs81gsjgs',
+  })
   @IsString()
   projectId: string;
 
+  @ApiProperty({
+    example: 10,
+  })
   @IsNumber()
   simulasiPerkiraanBungaKPR: number;
 
+  @ApiProperty({
+    example: 5,
+  })
   @IsNumber()
   simulasiLamaCicilanKPR: number;
 
