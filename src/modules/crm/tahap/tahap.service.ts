c76529f73191107879773b8e73233abb3dcfe827
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-03 13:50:42 +0700

    Add CRM validation before creating Tahap in findAll method

--- src/modules/crm/tahap/tahap.service.ts
+++ src/modules/crm/tahap/tahap.service.ts
@@ -1,4 +1,6 @@
+import { crmCreate, dbPocketbase } from 'src/common/helpers/crm.helper';
 import { CreateTahapDto, GetTahapDto } from './dto/tahap.dto';
+import { BadRequestException } from '@nestjs/common';
 
 export class TahapService {
   async findAll() {}
@@ -9,6 +11,29 @@ export class TahapService {
 
   async create(arg: CreateTahapDto) {
     const { projectId, stage } = arg;
-    
+    const findSameName = await dbPocketbase({
+      q: `
+      SELECT 
+        s."stageName"
+      FROM cms_stages s
+      JOIN cms_projects p ON s."projectId" = p.id
+      WHERE s."stageName" = '${stage}' AND s."projectId" = '${projectId}'
+      `,
+    });
+    if (findSameName) throw new BadRequestException(`Tahap ${stage} sudah ada`);
+
+    const createData = await crmCreate({
+      collection: 'cms_stages',
+      data: {
+        stageName: stage,
+        projectId,
+      },
+    });
+
+    return {
+      id: createData?.data?.id,
+      stage,
+      projectId,
+    };
   }
 }
