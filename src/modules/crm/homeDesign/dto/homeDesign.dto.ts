c8e67e8da430643fa5338af7f00f2523e3dd529e
SyahidbinFadlyl <ava.rageman18@gmail.com>
2024-10-04 01:30:21 +0700

    Simplify class-validator imports in homeDesign.dto.ts

--- src/modules/crm/homeDesign/dto/homeDesign.dto.ts
+++ src/modules/crm/homeDesign/dto/homeDesign.dto.ts
@@ -1,18 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsString,
-  IsEmail,
-  ArrayMinSize,
-  ArrayNotEmpty,
-  IsArray,
-  IsIn,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  ValidateNested,
-  IsBoolean,
-  Min,
-} from 'class-validator';
+import { IsString, IsArray, IsNotEmpty, IsNumber, Min } from 'class-validator';
 
 export class CreateHomeDesignDto {
   @ApiProperty({
